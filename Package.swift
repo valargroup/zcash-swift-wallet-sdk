f4ef4f5e0af9165ac59c3591212364157f54eafc
roman <roman@osmosis.team>
2026-05-11 21:55:26 -0300

    Point Package.swift at main TestFlight FFI artifact

--- Package.swift
+++ Package.swift
@@ -30,8 +30,8 @@ if useLocalFFI {
     targets.append(
         .binaryTarget(
             name: "libzcashlc",
-            url: "https://github.com/zcash/zcash-swift-wallet-sdk/releases/download/2.5.0/libzcashlc.xcframework.zip",
-            checksum: "1a38e5675f1015bb47006b17db33acbdc112549c9d8622e7b7918cec6152c2c8"
+            url: "https://github.com/valargroup/zcash-swift-wallet-sdk/releases/download/main-testflight-61821031/libzcashlc.xcframework.zip",
+            checksum: "fd601d0e387b000be352ca57239efaf787acf778c768c06120c1187890c2fcc0"
         )
     )
     sdkDependencies.append("libzcashlc")
